239b5ae19e96beaa480a8535935d0b4c3960ff88
Yoji Okada <yokada.moo.jp@gmail.com>
2018-01-14 23:12:43 +0900

    Implemented validations for blogs, portfolios and skills.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 255 }
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  validates :title, presence: true
+  validates :percent_utilized, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
 end
