f2e8a26e373aa08de6aade64f430f2ca1526b01e
Yoji Okada <yokada.moo.jp@gmail.com>
2018-01-14 09:08:24 +0900

    Integrated new and create actions for portofolio feature.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,58 @@
 class PortfoliosController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def show
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio is now live.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio was updated' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was removed'}
+      format.html { head :no_content }
+    end
+  end
+
+  private
+
+  def set_blog
+    @portfolio_item ||= Blog.find_by(id: params[:id])
+  end
+
+  def portfolio_item_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end
