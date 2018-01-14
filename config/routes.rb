f5123e9570de9295a93fec55a66407c5c040be36
Yoji Okada <yokada.moo.jp@gmail.com>
2018-01-14 10:39:44 +0900

    Configured custom routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#home', except: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :portfolios
 
